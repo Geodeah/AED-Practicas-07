bcb6a14c598f2931cf99b2ca0a4c4ab6a90b2a20
Geodeah <101605905+Geodeah@users.noreply.github.com>
2025-05-03 13:58:05 -0500

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,23 +2,23 @@ package Lab05practica.ejercicios;
 import java.util.List;
 
 public class Ejercicio1 {
-	// Método genérico para buscar un elemento en una lista
+	
     public static <T> boolean buscarElemento(List<T> lista, T valor) {
-        // Recorremos cada elemento de la lista
+      
         for (T elemento : lista) {
             if (elemento.equals(valor)) {
-                return true; // Si encontramos el valor, retornamos true
+                return true; 
             }
         }
-        return false; // Si no se encuentra el valor, retornamos false
+        return false; 
     }
 
-    // Método de prueba
+    
     public static void main(String[] args) {
         List<String> tareas = List.of("Comprar", "Estudiar", "Dormir");
 
-        System.out.println(buscarElemento(tareas, "Estudiar")); // true
-        System.out.println(buscarElemento(tareas, "Correr"));   // false
+        System.out.println(buscarElemento(tareas, "Estudiar")); 
+        System.out.println(buscarElemento(tareas, "Correr"));   
         System.out.println(buscarElemento(tareas, "Dormir"));    
     }
 }
