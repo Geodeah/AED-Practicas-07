9c321bab456e6b6a0391e171d98fb92ed8e4c57c
Geodeah <101605905+Geodeah@users.noreply.github.com>
2025-05-03 13:58:20 -0500

    Update Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -4,12 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Ejercicio2 {
-	// Método genérico que invierte una lista genérica y retorna una nueva lista
-	// invertida
+	
 	public static <T> List<T> invertirLista(List<T> listaOriginal) {
 		List<T> listaInvertida = new ArrayList<>();
 
-		// Recorremos la lista desde el final hasta el principio
+		
 		for (int i = listaOriginal.size() - 1; i >= 0; i--) {
 			listaInvertida.add(listaOriginal.get(i));
 		}
@@ -17,7 +16,7 @@ public class Ejercicio2 {
 		return listaInvertida;
 	}
 
-	// Método de prueba
+	
 	public static void main(String[] args) {
 		List<String> tareas = List.of("Comprar", "Estudiar", "Dormir");
 		List<String> tareasInvertidas = invertirLista(tareas);
